f70cb040c9358c8efcc1815f251ac7b4949c4ef7
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-09 15:43:49 -0800

    add panel

--- src/routes/build.tsx
+++ src/routes/build.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Panel } from "../Panel";
+import clsx from "clsx";
 
 export const Route = createFileRoute("/build")({
   component: BuildComponent,
@@ -7,8 +9,13 @@ export const Route = createFileRoute("/build")({
 function BuildComponent() {
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center pointer-events-none">
-        <div className="text-2xl">hello world</div>
+      <div
+        className={clsx(
+          "fixed inset-0 pointer-events-none",
+          "p-4 flex flex-col-reverse",
+        )}
+      >
+        <Panel className="p-4">Test</Panel>
       </div>
     </>
   );
