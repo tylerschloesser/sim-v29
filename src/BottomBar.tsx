bc9eef21616ba22b46afb7c8df9ff9f0f620ba2d
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 16:53:50 -0800

    add clsx

--- src/BottomBar.tsx
+++ src/BottomBar.tsx
@@ -1,14 +1,23 @@
+import clsx from "clsx";
 import { times } from "lodash-es";
+
+const config: "bottom" | "left" | "right" = "left";
 
 export function BottomBar() {
   return (
     <div className="fixed inset-x-0 bottom-0">
       <div className="p-4">
-        <div className="border rounded overflow-hidden grid grid-cols-5 grid-rows-2">
+        <div
+          className={clsx("border rounded overflow-hidden", {
+            "grid grid-cols-5 grid-rows-2": config === "bottom",
+            // prettier-ignore
+            "inline-grid grid-cols-2 grid-rows-5": config === "left" || config === "right",
+          })}
+        >
           {times(10, (i) => (
             <button
               key={i}
-              className="py-4"
+              className="p-4"
               onClick={() => alert(`Button ${i + 1} clicked`)}
             >
               {i + 1}
