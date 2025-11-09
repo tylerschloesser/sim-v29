022fb04de6b290712cdc7649e89d13f4e51ba177
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 18:25:12 -0800

    move import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { App } from "./App.tsx";
 import invariant from "tiny-invariant";
 import { setupPixi } from "./pixi.ts";
+
+import "./index.css";
 
 const canvas = document.querySelector("canvas");
 invariant(canvas);
