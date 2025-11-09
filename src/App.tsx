e386fd0ba4f3ac4047d0bb8fba169be38f6414ff
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 16:33:27 -0800

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,29 @@ export function App() {
   return (
     <div className="fixed inset-0 top-[unset]">
       <div className="p-4">
-        <div className="border rounded flex divide-x">
-          {times(5, (i) => (
-            <button
-              key={i}
-              className="flex-1 p-2 aspect-square"
-              onClick={() => alert(`Button ${i + 1} clicked`)}
-            >
-              {i + 1}
-            </button>
-          ))}
+        <div className="border rounded flex flex-col divide-y">
+          <div className="flex divide-x">
+            {times(5, (i) => (
+              <button
+                key={i}
+                className="flex-1 p-2 aspect-square"
+                onClick={() => alert(`Button ${i + 1} clicked`)}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </div>
+          <div className="flex divide-x">
+            {times(5, (i) => (
+              <button
+                key={i}
+                className="flex-1 p-2 aspect-square"
+                onClick={() => alert(`Button ${i + 6} clicked`)}
+              >
+                {i + 6}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
