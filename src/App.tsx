bba9760f995d68425a208fac7378479b3ff20d50
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 16:13:44 -0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import { times } from "lodash-es";
 
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="fixed inset-0 top-[unset]">
-        <div className="p-4">
-          <div className="border rounded flex">
-            {times(5, (i) => (
-              <button key={i} className="flex-1 p-2 aspect-square">
-                {i + 1}
-              </button>
-            ))}
-          </div>
+    <div className="fixed inset-0 top-[unset]">
+      <div className="p-4">
+        <div className="border rounded flex divide-x">
+          {times(5, (i) => (
+            <button key={i} className="flex-1 p-2 aspect-square">
+              {i + 1}
+            </button>
+          ))}
         </div>
       </div>
     </div>
