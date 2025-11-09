bce9d89a52526a9b4c0e42314cb6e378d1d006da
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 16:58:27 -0800

    cleanup

--- src/BottomBar.tsx
+++ src/BottomBar.tsx
@@ -1,15 +1,20 @@
 import clsx from "clsx";
 import { times } from "lodash-es";
 
-const config: "bottom" | "left" | "right" = "left";
+const config: "bottom" | "left" | "right" = "right";
 
 export function BottomBar() {
   return (
     <div className="fixed inset-x-0 bottom-0">
-      <div className="p-4">
+      <div
+        className={clsx("flex p-4", {
+          "justify-end": config === "right",
+          "justify-start": config === "left",
+        })}
+      >
         <div
-          className={clsx("border rounded overflow-hidden", {
-            "grid grid-cols-5 grid-rows-2": config === "bottom",
+          className={clsx("border rounded", {
+            "flex-1 grid grid-cols-5 grid-rows-2": config === "bottom",
             // prettier-ignore
             "inline-grid grid-cols-2 grid-rows-5": config === "left" || config === "right",
           })}
