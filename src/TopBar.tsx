ed3d233ae1c1ae38d2d4d84dfb454de9297e8d6e
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-09 08:53:25 -0800

    render tile

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,10 +1,24 @@
 import { Panel } from "./Panel";
+import { useHighlightedTile } from "./useHighlightedTile";
 
 export function TopBar() {
+  const { tileX, tileY, resource } = useHighlightedTile();
+
   return (
     <div className="fixed inset-x-0 top-0">
       <div className="p-4">
-        <Panel className="p-4">Hello world</Panel>
+        <Panel className="p-4">
+          <div className="flex gap-4">
+            <div>
+              Tile: ({tileX}, {tileY})
+            </div>
+            {resource && (
+              <div>
+                Resource: {resource.type} ({resource.count})
+              </div>
+            )}
+          </div>
+        </Panel>
       </div>
     </div>
   );
