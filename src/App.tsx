ed4759ec715831ff4d31597689e4d69a83367b64
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-08 16:16:40 -0800

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ export function App() {
       <div className="p-4">
         <div className="border rounded flex divide-x">
           {times(5, (i) => (
-            <button key={i} className="flex-1 p-2 aspect-square">
+            <button
+              key={i}
+              className="flex-1 p-2 aspect-square"
+              onClick={() => alert(`Button ${i + 1} clicked`)}
+            >
               {i + 1}
             </button>
           ))}
