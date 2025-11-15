8d578e772cc75c2f46a93f88ad7df854c0a85c46
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-15 08:43:13 -0800

    show icons in build

--- src/SelectEntityPanel.tsx
+++ src/SelectEntityPanel.tsx
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { inventoryHas } from "./inventoryUtils";
 import { Panel } from "./Panel";
-import { ENTITY_TYPES, type EntityType, type Inventory } from "./types";
+import {
+  ENTITY_CONFIGS,
+  ENTITY_TYPES,
+  type EntityType,
+  type Inventory,
+} from "./types";
 
 interface SelectEntityPanelProps {
   inventory: Inventory;
@@ -14,16 +20,22 @@ export function SelectEntityPanel({
   return (
     <Panel className="p-4">
       <div className="flex flex-col gap-2">
-        {ENTITY_TYPES.map((entityType) => (
-          <button
-            key={entityType}
-            disabled={!inventoryHas(inventory, entityType)}
-            onClick={() => onSelectEntity(entityType)}
-            className="px-4 py-2 bg-blue-500 text-white disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            {entityType}
-          </button>
-        ))}
+        {ENTITY_TYPES.map((entityType) => {
+          const config = ENTITY_CONFIGS[entityType];
+          return (
+            <button
+              key={entityType}
+              disabled={!inventoryHas(inventory, entityType)}
+              onClick={() => onSelectEntity(entityType)}
+              className="px-4 py-2 bg-blue-500 text-white disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              <div className="flex items-center justify-between gap-2">
+                <FontAwesomeIcon icon={config.icon} />
+                <span>{entityType}</span>
+              </div>
+            </button>
+          );
+        })}
       </div>
     </Panel>
   );
