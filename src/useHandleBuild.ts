3b4d188572281923212ec885d8b63737deeb13a8
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-21 07:15:10 -0800

    cleanup

--- src/useHandleBuild.ts
+++ src/useHandleBuild.ts
@@ -1,7 +1,6 @@
-import { invariant } from "./invariant";
 import { useAppContext } from "./appContext";
-import { getAdjacentBelts } from "./beltUtils";
 import { getTilesForEntity } from "./entityUtils";
+import { invariant } from "./invariant";
 import { decrementInventory } from "./inventoryUtils";
 import type { Entity } from "./types";
 import { CHUNK_SIZE, getChunkId, getEntityId, tileToChunk } from "./types";
@@ -29,62 +28,6 @@ export function useHandleBuild(): (entity: Entity) => void {
 
       // Add entity to entities Map
       draft.entities.set(entityId, updatedEntity);
-
-      // Log belt connections if this is a belt
-      if (updatedEntity.type === "belt") {
-        const adjacent = getAdjacentBelts(updatedEntity, draft);
-        console.log("Belt placed:", {
-          position: updatedEntity.position,
-          rotation: updatedEntity.rotation,
-          turn: updatedEntity.turn,
-          adjacent: {
-            top: {
-              tile: adjacent.top.tile,
-              belt: adjacent.top.belt
-                ? {
-                    id: adjacent.top.belt.id,
-                    position: adjacent.top.belt.position,
-                    rotation: adjacent.top.belt.rotation,
-                    turn: adjacent.top.belt.turn,
-                  }
-                : null,
-            },
-            right: {
-              tile: adjacent.right.tile,
-              belt: adjacent.right.belt
-                ? {
-                    id: adjacent.right.belt.id,
-                    position: adjacent.right.belt.position,
-                    rotation: adjacent.right.belt.rotation,
-                    turn: adjacent.right.belt.turn,
-                  }
-                : null,
-            },
-            bottom: {
-              tile: adjacent.bottom.tile,
-              belt: adjacent.bottom.belt
-                ? {
-                    id: adjacent.bottom.belt.id,
-                    position: adjacent.bottom.belt.position,
-                    rotation: adjacent.bottom.belt.rotation,
-                    turn: adjacent.bottom.belt.turn,
-                  }
-                : null,
-            },
-            left: {
-              tile: adjacent.left.tile,
-              belt: adjacent.left.belt
-                ? {
-                    id: adjacent.left.belt.id,
-                    position: adjacent.left.belt.position,
-                    rotation: adjacent.left.belt.rotation,
-                    turn: adjacent.left.belt.turn,
-                  }
-                : null,
-            },
-          },
-        });
-      }
 
       // Update all tiles occupied by entity to reference it
       for (const { x, y } of getTilesForEntity(updatedEntity)) {
