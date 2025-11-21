3e4795e1930b6073e8aa3e5e318a4c5f503eb428
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-21 08:25:26 -0800

    refactor entities

--- src/useBuildPreview.ts
+++ src/useBuildPreview.ts
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { isBelt, type BuildRouteSearch } from "./build-types";
 import { getRotatedSize, getTilesForEntity } from "./entityUtils";
+import { invariant } from "./invariant";
 import type { PixiController } from "./PixiController";
 import { getTileAtCoords } from "./tileUtils";
 import type {
@@ -18,7 +19,6 @@ import {
   getEntityId,
   TILE_SIZE,
 } from "./types";
-import { invariant } from "./invariant";
 
 /**
  * Hook that manages the build preview based on camera position and selected entity type.
@@ -44,67 +44,77 @@ export function useBuildPreview(
     const entityX = Math.round(state.camera.x / TILE_SIZE - rotatedSize.x / 2);
     const entityY = Math.round(state.camera.y / TILE_SIZE - rotatedSize.y / 2);
 
-    let entity: Entity;
+    const entities: Entity[] = [];
     if (isBelt(search)) {
       if (search.sourceId === null) {
         const entityId = getEntityId(state.nextEntityId);
-        entity = createBeltEntity(
-          entityId,
-          search.selectedEntityType,
-          entityX,
-          entityY,
-          rotation,
-          search.turn,
+        entities.push(
+          createBeltEntity(
+            entityId,
+            search.selectedEntityType,
+            entityX,
+            entityY,
+            rotation,
+            search.turn,
+          ),
         );
       } else {
         // TODO
         const entityId = getEntityId(state.nextEntityId);
-        entity = createBeltEntity(
-          entityId,
-          search.selectedEntityType,
-          entityX,
-          entityY,
-          rotation,
-          search.turn,
+        entities.push(
+          createBeltEntity(
+            entityId,
+            search.selectedEntityType,
+            entityX,
+            entityY,
+            rotation,
+            search.turn,
+          ),
         );
       }
     } else {
       const entityId = getEntityId(state.nextEntityId);
-      entity = createEntity(
-        entityId,
-        search.selectedEntityType,
-        entityX,
-        entityY,
-        search.rotation,
+      entities.push(
+        createEntity(
+          entityId,
+          search.selectedEntityType,
+          entityX,
+          entityY,
+          search.rotation,
+        ),
       );
     }
 
-    // Check validity: entity is valid if all tiles it covers don't have an entityId
-    const entityTiles = getTilesForEntity(entity).map(({ x, y }) =>
-      getTileAtCoords(state, x, y),
-    );
-    const valid = entityTiles.every((tile) => !tile?.entityId);
-
-    if (
-      !valid &&
-      search.selectedEntityType === "belt" &&
-      search.sourceId === null
-    ) {
+    if (search.selectedEntityType === "belt" && search.sourceId === null) {
       // check if placing the belt here would connect to any existing belts
+      invariant(entities.length === 1);
+      const entity = entities.at(0);
+      invariant(entity);
+      const entityTiles = getTilesForEntity(entity).map(({ x, y }) =>
+        getTileAtCoords(state, x, y),
+      );
       invariant(entityTiles.length === 1);
       const tile = entityTiles.at(0);
-      invariant(tile?.entityId);
-      const existingEntity = state.entities.get(tile.entityId);
-      invariant(existingEntity);
-      if (existingEntity.type === "belt") {
-        return {
-          type: "start-belt",
-          entities: [existingEntity],
-        } satisfies StartBeltBuild;
+      if (tile?.entityId) {
+        const existingEntity = state.entities.get(tile.entityId);
+        invariant(existingEntity);
+        if (existingEntity.type === "belt") {
+          return {
+            type: "start-belt",
+            entities: [existingEntity],
+          } satisfies StartBeltBuild;
+        }
       }
     }
 
-    return { type: "simple", entities: [entity], valid } satisfies SimpleBuild;
+    const valid = entities.every((entity) => {
+      const entityTiles = getTilesForEntity(entity).map(({ x, y }) =>
+        getTileAtCoords(state, x, y),
+      );
+      return entityTiles.every((tile) => !tile?.entityId);
+    });
+
+    return { type: "simple", entities, valid } satisfies SimpleBuild;
   }, [state, search]);
 
   useEffect(() => {
